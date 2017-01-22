b487eafbd47d8a48654693590f47bdc12ebdb2a5
Jeremy Bellows <JeremyBellows@gmail.com>
2017-01-22 11:14:22 -0700

    Actuator - added test for two inbound connections

--- test/actuator_test.exs
+++ test/actuator_test.exs
@@ -55,4 +55,39 @@ defmodule Evolixir.ActuatorTest do
     assert activated_value == artificial_synapse.value
   end
 
+  test ":receive_synapse should activate Actuator if barrier is full with two expected inbound connections" do
+    {:ok, actuator_test_helper_pid} = GenServer.start_link(NodeTestHelper, %NodeTestHelper{})
+
+    activate_function =
+    fn output_value ->
+      :ok = GenServer.call(actuator_test_helper_pid, {:activate, output_value})
+    end
+
+    {:ok, actuator_pid} = GenServer.start_link(Actuator, %Actuator{activate_function: activate_function})
+
+    fake_node_pid = 9
+    {:ok, connection_id} = GenServer.call(actuator_pid, {:add_inbound_connection, fake_node_pid})
+    {:ok, connection_id_two} = GenServer.call(actuator_pid, {:add_inbound_connection, fake_node_pid})
+
+    artificial_synapse = %Synapse{
+      connection_id: connection_id,
+      from_node_id: fake_node_pid,
+      value: 1.5
+    }
+    artificial_synapse_two = %Synapse{
+      connection_id: connection_id_two,
+      from_node_id: fake_node_pid,
+      value: 4.9
+    }
+    GenServer.cast(actuator_pid, {:receive_synapse, artificial_synapse})
+    GenServer.cast(actuator_pid, {:receive_synapse, artificial_synapse_two})
+
+    :timer.sleep(5)
+    updated_test_state = GenServer.call(actuator_test_helper_pid, :get_state)
+
+    {true, activated_value} = updated_test_state.was_activated
+    expected_value = artificial_synapse.value + artificial_synapse_two.value
+    assert_in_delta activated_value, expected_value, 0.001
+  end
+
 end

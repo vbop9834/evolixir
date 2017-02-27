0cbb7406af63b02c387ff60bcabb6636d18f669d
Jeremy Bellows <JeremyBellows@gmail.com>
2017-02-27 10:35:45 -0700

    Stochastic Hill Climber - fixed list range

--- lib/stochastic_hill_climber.ex
+++ lib/stochastic_hill_climber.ex
@@ -107,16 +107,9 @@ defmodule StochasticHillClimber do
           network_size_max_attempts
       end
 
-    case max_attempts do
-      1 ->
-        perturb_id = 1
-        neurons = perturb_weights_in_neurons(neurons, probability_of_neuron_perturbing)
-        [{perturb_id, {sensors, neurons, actuators}}]
-      max_attempts ->
-        Enum.map([1..max_attempts], fn perturb_id ->
-          neurons = perturb_weights_in_neurons(neurons, probability_of_neuron_perturbing)
-          {perturb_id, {sensors, neurons, actuators}}
-        end)
-    end
+    Enum.map(Enum.to_list([1..max_attempts]), fn perturb_id ->
+      neurons = perturb_weights_in_neurons(neurons, probability_of_neuron_perturbing)
+      {perturb_id, {sensors, neurons, actuators}}
+    end)
   end
 end
